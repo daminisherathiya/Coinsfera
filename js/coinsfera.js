d9d04aa6f0bc98e525d8f821404bb9552549ceb1
Damini Sherathiya <daminisherathiya@gmail.com>
2020-12-18 11:26:33 +0530

    Fixes.

--- js/coinsfera.js
+++ js/coinsfera.js
@@ -1,20 +1,22 @@
 var banner_imgs = ["./images/banner-img1.png", "./images/banner-img2.png", "./images/banner-img3.png"];
 var banner_imgs_idx = 0;
+var prev_arrow;
+var next_arrow;
 
-$(function () {
+// $(function () {
   show_banners();
-});
+// });
 
 function show_banners() {
-  var prev_arrow = document.querySelector(".prev");
-  var next_arrow = document.querySelector(".next");
+  prev_arrow = document.querySelector(".prev");
+  next_arrow = document.querySelector(".next");
   prev_arrow.addEventListener("click", event_listner_for_prev_arrow);
   next_arrow.addEventListener("click", event_listner_for_next_arrow);
 }
 
 function event_listner_for_prev_arrow() {
   var img = document.querySelector(".address-img");
-  banner_imgs_idx = (banner_imgs_idx - 1 + 3) % 3;
+  banner_imgs_idx = (banner_imgs_idx - 1);
   img.setAttribute("src", banner_imgs[banner_imgs_idx]);
 }
 
