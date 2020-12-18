c227644ec4c3c0bb89728dd3ba8400e9072fdf8e
Damini Sherathiya <daminisherathiya@gmail.com>
2020-12-18 11:07:04 +0530

    Added javascript file.

---
+++ js/coinsfera.js
@@ -0,0 +1,25 @@
+var banner_imgs = ["./images/banner-img1.png", "./images/banner-img2.png", "./images/banner-img3.png"];
+var banner_imgs_idx = 0;
+
+$(function () {
+  show_banners();
+});
+
+function show_banners() {
+  var prev_arrow = document.querySelector(".prev");
+  var next_arrow = document.querySelector(".next");
+  prev_arrow.addEventListener("click", event_listner_for_prev_arrow);
+  next_arrow.addEventListener("click", event_listner_for_next_arrow);
+}
+
+function event_listner_for_prev_arrow() {
+  var img = document.querySelector(".address-img");
+  banner_imgs_idx = (banner_imgs_idx - 1 + 3) % 3;
+  img.setAttribute("src", banner_imgs[banner_imgs_idx]);
+}
+
+function event_listner_for_next_arrow() {
+  var img = document.querySelector(".address-img");
+  banner_imgs_idx = (banner_imgs_idx + 1) % 3;
+  img.setAttribute("src", banner_imgs[banner_imgs_idx]);
+}
